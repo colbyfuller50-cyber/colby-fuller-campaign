a300d68c2f442c99029f85743af3469ac1ed757a
colbyfuller50-cyber <colbyfuller50@gmail.com>
2026-03-07 13:17:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+nav.classList.toggle("active");
+});
